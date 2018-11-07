bbe72eeac8b8f7cd3c68d6792ada48d30ccd07cc
Kevin Sheppard <ksheppard@mpu.gov.tt>
2018-11-07 13:31:18 -0400

    Removed old build files

---
+++ src/test/groovy/com/boltframework/web/routing/ControllerCollectionTest.java
@@ -0,0 +1,24 @@
+package com.boltframework.web.routing;
+
+import app.controllers.Application;
+import com.boltframework.web.routing.annotations.Get;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ControllerCollectionTest {
+  Logger logger = LoggerFactory.getLogger(getClass());
+
+  @Test
+  public void get() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
+    Method method = Application.class.getDeclaredMethod("_get");
+    Annotation annotation = method.getAnnotation(Get.class);
+    logger.debug("Annotation simple name is: {}, Route value is: {}", annotation.annotationType().getSimpleName(), annotation.annotationType().getMethod("value").invoke(annotation));
+    logger.debug(String[].class.getSimpleName());
+  }
+
+}
