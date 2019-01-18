6299be6a238ac7ce4dcdbc40a6cf2149a78f7def
Kevin Sheppard <kevin@reviselabs.com>
2019-01-18 08:31:17 -0400

    Cleaned up the ThreadLocal test

--- src/test/groovy/com/boltframework/test/ThreadLocalTest.groovy
+++ src/test/groovy/com/boltframework/test/ThreadLocalTest.groovy
@@ -1,61 +1,54 @@
 package com.boltframework.test
 
-import org.junit.Before
 import org.junit.Test
 
+import static org.junit.Assert.assertEquals
+
+/**
+ * A test class created to understand the concept of ThreadLocals and how to use them.
+ */
 class ThreadLocalTest {
-
-  Controller controller
-
-  @Before
-  public void setUp() throws Exception {
-    controller = new Controller()
-  }
-
-  Controller getController() {
-    return controller
-  }
 
   @Test
   void basicTest() {
-    // User A makes a request that takes a while
-    def _controller = getController()
-    _controller.threadSafeContext.set("Bar")
-    _controller.nonThreadSafeContext = "Original value"
-    // User B makes a request that completes immediately
+    final Controller controller = new Controller()
+
+    // User A
     Thread thread1 = new Thread({
-      def _controller2 = getController()
-      _controller2.threadSafeContext.set("Baz")
-      _controller.nonThreadSafeContext = "Overwritten by Thread 1"
-      println("Thread 1: ${_controller.threadSafeContext.get()}")
-      println("Thread 1: ${_controller.nonThreadSafeContext}")
+      controller.threadSafeContext.set("Baz")
+      controller.nonThreadSafeContext = "Overwritten by Thread 1"
+      println("Thread 1: ${controller.threadSafeContext.get()}")
+      println("Thread 1: ${controller.nonThreadSafeContext}")
+      assertEquals('Baz', controller.threadSafeContext.get())
     })
+
+    // User B
     Thread thread2 = new Thread({
-      def _controller2 = getController()
-      _controller2.threadSafeContext.set("Biff")
-      _controller.nonThreadSafeContext = "Overwritten by Thread 2"
-      println("Thread 2: ${_controller.threadSafeContext.get()}")
-      println("Thread 2: ${_controller.nonThreadSafeContext}")
+      controller.threadSafeContext.set("Biff")
+      controller.nonThreadSafeContext = "Overwritten by Thread 2"
+      println("Thread 2: ${controller.threadSafeContext.get()}")
+      println("Thread 2: ${controller.nonThreadSafeContext}")
       sleep(200)
+      assertEquals('Biff', controller.threadSafeContext.get())
     })
+
+    // User A makes a request that completes immediately
     thread1.start()
+
+    // User B makes a request that takes a while
     thread2.start()
+
+    // User C makes a request that takes the longest
     Thread.sleep(300)
-    println("Main thread: ${_controller.threadSafeContext.get()}")
-    println("Main thread: ${_controller.nonThreadSafeContext}")
+    assertEquals('Bar', controller.threadSafeContext.get())
+    assertEquals('Overwritten by Thread 2', controller.nonThreadSafeContext)
+    println("Main thread: ${controller.threadSafeContext.get()}")
+    println("Main thread: ${controller.nonThreadSafeContext}")
 
   }
 
   static class Controller {
     ThreadLocal<String> threadSafeContext = new ThreadLocal<>()
     String nonThreadSafeContext
-
-    public String context() {
-      return threadSafeContext.get()
-    }
-
-    public void context(String ctx) {
-      threadSafeContext.set(ctx)
-    }
   }
 }
