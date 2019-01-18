e86bfab060cd183ae1593b85870c7e2d6a14bc61
Kevin Sheppard <kevin@reviselabs.com>
2019-01-18 07:33:57 -0400

    Changed the return type to Interceptor

--- src/main/java/com/boltframework/utils/Middleware.java
+++ src/main/java/com/boltframework/utils/Middleware.java
@@ -1,14 +1,12 @@
 package com.boltframework.utils;
 
-import com.boltframework.web.HttpContext;
+import com.boltframework.web.mvc.Interceptor;
 import io.vertx.core.Handler;
 import io.vertx.ext.web.RoutingContext;
 
-import java.util.function.Consumer;
-
 public class Middleware {
 
-  public static Consumer<HttpContext> convertVertxHandler(Handler<RoutingContext> handler) {
+  public static Interceptor convertVertxHandler(Handler<RoutingContext> handler) {
     return ctx -> handler.handle(ctx.getDelegate());
   }
 }
